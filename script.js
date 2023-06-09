866da1983c5283d62e082219c2880969ac96d2e8
Nicolas <nccrosetti@gmail.com>
2023-06-09 16:24:09 -0300

    vertical images

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 window.onload = function() {
     new Glide('.glide', {
       type: 'carousel',
-      perView: 3,
+      perView: 4.5,
       focusAt: 'center',
       gap: 40,
       breakpoints: {
